a38c07e2463b38b44e3a418ae16db7fba2b765e9
Aaron G <ag505181@gmail.com>
2019-10-15 15:05:37 -0700

    move data to top level of the app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 import CardList from "./Component/CardList";
 import Form from "./Component/Form"
 
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+]
+
 class App extends React.Component {
+  state = {
+    profiles: testData
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">{this.props.title}</header>
         <Form />
-        <CardList />
+        <CardList profiles={this.state.profiles}/>
       </div>
     );
   }
--- src/Component/CardList.js
+++ src/Component/CardList.js
@@ -6,14 +6,9 @@ import Card from "./Card";
 class CardList extends React.Component {
   
   render() {
-    const testData = [
-			{name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-      {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  		{name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-	];
     return (
      <div> 
-     {testData.map(
+     {this.props.profiles.map(
         profile => 
         <Card key={profile.name} {...profile} />)
     }
