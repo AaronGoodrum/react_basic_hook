50a597504776163a7f520e84175d80c979597d6d
Aaron G <ag505181@gmail.com>
2019-10-15 14:07:05 -0700

    passing data from comp to other

--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 class Card extends React.Component {
-  
+
   render() {
+    const profile = this.props.testData[0]
     return (
       <div className="github-profile">
-        <img alt="headshots" src="https://placehold.it/75" />
+        <img alt={profile.name} src={profile.avatar_url} />
         <div className="info">
-          <div className="name"> Name here...</div>
-          <div className="company">Company name...</div>
+          <div className="name"> {profile.name}</div>
+          <div className="company">{profile.company}</div>
         </div>
       </div>
     )
