d2b752fa4f5484ee401a359790a73d089e2e1269
Aaron G <ag505181@gmail.com>
2019-10-15 15:01:28 -0700

    add form input for search

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import CardList from "./Component/CardList";
+import Form from "./Component/Form"
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">{this.props.title}</header>
+        <Form />
         <CardList />
       </div>
     );
