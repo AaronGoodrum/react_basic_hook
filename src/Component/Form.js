1ed33e0070c43564934be6ed1ab547ea0a57ed2d
Aaron <ag505181@gmail.com>
2019-10-15 19:11:53 -0700

    taking input from the user form

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 class Form extends React.Component {
+  state = {userName:''}
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(
+      this.state.userName
+    )
+  }
   render() {
     return (
-      <form action="">
-        <input type="text" placeholder="Github search"></input>
-        <button>Add Card</button>
-      </form>
+      <form onSubmit={this.handleSubmit}>
+      <input
+        type="text" placeholder="Github search" 
+        onChange={
+          event => this.setState({
+            userName: event.target.value
+          })
+        } 
+        required>
+      </input>
+      <button>Add Card</button>
+      </form >
     );
   }
 }
